48ec3b0af70e58a218c292e7e784650e066ea590
mamata pandey <mamata@Mac.Home>
2026-01-24 00:14:14 +0000

    update handle Func

--- src/server/routeMessage.ts
+++ src/server/routeMessage.ts
@@ -1,17 +1,13 @@
-import { WebSocket } from "ws";
-import { sendMessage } from "../util/sendMessage";
-import {
-  // chatCompletionInStream,
-  getLLMTextResponse,
-  // createLLMTextStream,
-} from "../services/llmService";
+import { WebSocket, RawData } from "ws";
+import { sendMessage } from "../utils/sendMessage";
+// import { streamLLMToTTS } from "../services/llmService";
+import { catchAsynchError } from "../utils/catchAsyncError";
+import { streamLLMToTTS } from "../orchestrateServices/streamLLMtoTTS";
 
 export const handleMessage = async (session: any, rawData: any) => {
   try {
     const data = JSON.parse(rawData);
-
-    const text = await getLLMTextResponse(data);
-    sendMessage(session.ws, text);
+    const msg = await streamLLMToTTS(data.message, session);
   } catch (err) {
     console.log(err);
     sendMessage(session.ws, JSON.stringify({ message: "invalid JSON" }));

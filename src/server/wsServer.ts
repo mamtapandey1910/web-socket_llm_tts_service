cf21df10952992d719b3c37bd8641b66201f9e61
mamata pandey <mamata@Mac.Home>
2026-01-25 22:07:15 +0000

    add more socket handler

--- src/server/wsServer.ts
+++ src/server/wsServer.ts
@@ -1,9 +1,18 @@
 import { WebSocketServer, WebSocket } from "ws";
 import { Request } from "express";
 import { handleWSConnection } from "./connection";
+import { sendError } from "../utils/error";
 
 export const sockerServer = (appServer: any) => {
-  const wss = new WebSocketServer({ server: appServer });
+  try {
+    const wss: WebSocketServer = new WebSocketServer({ server: appServer });
 
-  wss.on("connection", handleWSConnection);
+    wss.on("connection", handleWSConnection);
+
+    wss.on("error", (err) => {
+      console.log("Error occured in Websocket connection");
+    });
+  } catch (err: unknown) {
+    console.log("something went wrong globally", err);
+  }
 };

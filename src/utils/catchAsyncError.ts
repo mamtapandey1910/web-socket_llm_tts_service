295095962ab2ad4d2cbcbbfa088b52edb43e647f
mamata pandey <mamata@Mac.Home>
2026-01-25 21:34:12 +0000

    improve error handling func

--- src/utils/catchAsyncError.ts
+++ src/utils/catchAsyncError.ts
@@ -1,14 +1,23 @@
+import { WebSocket } from "ws";
 import { CustomError } from "./error";
+import { sendError, errorCodes } from "./error";
 
 export const catchSocketAsynchAsynchError = (
   func: (...args: any[]) => Promise<any>,
+  ws?: WebSocket,
+  errorType?: any,
 ) => {
   return async (...args: any) => {
     try {
       await func(...args);
-    } catch (error: any) {
-      console.error("Error: ", error.message);
-      throw new CustomError("Error: ", error.message);
+    } catch (error: unknown) {
+      if (ws && ws?.readyState === ws?.OPEN && error instanceof Error) {
+        sendError(ws, errorType, error.message);
+      } else {
+        if (error instanceof Error) {
+          throw new CustomError(error.message, 500);
+        }
+      }
     }
   };
 };
